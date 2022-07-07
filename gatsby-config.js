c27b0b39b9be93c710094add8af994493e27060f
ohyunkyo <ohyunkyo.dev@gmail.com>
2022-07-07 17:34:41 +0900

    페이지 가로 크기 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 630,
+              maxWidth: 950,
             },
           },
           {
